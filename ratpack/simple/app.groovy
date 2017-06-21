4b0cd8c30db6b65a97ba02ffd0d24159793036a1
Daewon Choe <daewon.choe@target.com>
2017-06-20 21:19:04 -0700

    add comments

--- ratpack/simple/app.groovy
+++ ratpack/simple/app.groovy
@@ -7,18 +7,22 @@ ratpack {
     development true
   }
   handlers {
+    // path: http://localhost:5050/
     get {
       render "hello world"
     }
 
+    // path: http://localhost:5050/ping
     get("ping") {
       render "ping"
     }
- 
+
+    // path: http://localhost:5050/name/<any value> 
     get("name/:name") {
       render "hello world, $pathTokens.name"
     }
-   
+  
+    // path: http://localhost:5050/status 
     path("status") {
       byMethod {
         get {
